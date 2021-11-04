835e8e144a6f9d3651660fa08c39afe14e6be9ef
Manu Sporny <msporny@digitalbazaar.com>
2021-11-04 11:40:51 -0400

    Add ReSpec plugin to generate DID Method table from JSON data.

---
+++ common.js
@@ -0,0 +1,34 @@
+/* Build DID Method table using JSON data */
+async function buildDidMethodTables(config) {
+  const {document} = window;
+  const response = await fetch('methods/index.json');
+  const allMethods = await response.json();
+
+  // set up the API summary table headers
+  const table = document.getElementById('did-method-table');
+  const tableHeader = document.createElement('thead');
+  tableHeader.innerHTML = '<th>DID&nbsp;Method</th><th>Registry</th>' +
+    '<th>Contact</th>';
+  table.appendChild(tableHeader);
+
+  // summarize each API endpoint
+  const tableBody = document.createElement('tbody');
+  for(const method of allMethods) {
+    const tableRow = document.createElement('tr');
+    const {name, verifiableDataRegistry, contactEmail, contactName,
+      contactWebsite, specification} = method;
+    let contactInfo = contactName;
+    if(contactEmail) {
+      contactInfo += ` (<a href="mailto:${contactEmail}">email</a>)`;
+    }
+    if(contactWebsite) {
+      contactInfo += ` (<a href="${contactWebsite}">website</a>)`;
+    }
+    tableRow.innerHTML =
+      `<td><a href="${specification}">${name}</a></td>` +
+      `<td>${verifiableDataRegistry}</td>` +
+      `<td>${contactInfo}</td>`;
+    tableBody.appendChild(tableRow);
+  }
+  table.appendChild(tableBody);
+}
